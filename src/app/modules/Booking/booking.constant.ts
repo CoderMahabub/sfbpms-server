ae85daa05a4f48557f69849ab036973f227bc54a
CoderMahabub <CoderMahabub@gmail.com>
2024-06-20 02:24:49 +0900

    Booking Interface

---
+++ src/app/modules/Booking/booking.constant.ts
@@ -0,0 +1,7 @@
+export const BOOKING_STATUS = {
+  confirmed: "confirmed",
+  unconfirmed: "unconfirmed",
+  canceled: "canceled",
+} as const;
+
+export const bookingSearchableFields = ["date"];
---
+++ src/app/modules/Booking/booking.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose";
+import { BOOKING_STATUS } from "./booking.constant";
+
+export type TBooking = {
+  date: string;
+  startTime: string;
+  endTime: string;
+  user: Types.ObjectId;
+  facility: Types.ObjectId;
+  payableAmount?: number;
+  isBooked: keyof typeof BOOKING_STATUS;
+};
+
+export type TSchedule = {
+  date: string;
+  startTime: string;
+  endTime: string;
+};
